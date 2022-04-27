aa6a887965e937829a7b7cb6f2283c7742806c08
Jeremy <asirwaadj@gmail.com>
2022-04-28 01:41:18 +0530

    done changes

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 5000 || process.env.PORT;
-const cors = require('cors');
-const bodyparser = require('body-parser');
-const mongoose = require('mongoose');
+const port = Process.env.PORT || 3000;
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 
+mongoose.connect(
+	"mongodb+srv://jeremy:jeremy4755@cluster0.byg43.mongodb.net/TNEA?retryWrites=true&w=majority",
+	() => {
+		console.log("MongoDB connected");
+	}
+);
 
-
-mongoose.connect("mongodb+srv://jeremy:jeremy4755@cluster0.byg43.mongodb.net/TNEA?retryWrites=true&w=majority",() => {
-    console.log("MongoDB connected");
-})
-
-app.use(cors())
-app.use(bodyparser.urlencoded({ extended: true }))
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
-app.use('/',require('./Routes/user.routes'))
-
+app.use("/", require("./Routes/user.routes"));
 
 app.listen(port, () => {
-    console.log("Post Running on " + port );
-})
+	console.log("Post Running on " + port);
+});
